b2037a1561ad9b9952de47551c51f70fefe7ceb7
Hassan0Anwer <153855531+Hassan0Anwer@users.noreply.github.com>
2024-03-01 01:53:29 +0500

    if else condition
    if else condition

---
+++ app.js
@@ -0,0 +1,19 @@
+var time = prompt("Hey Whats times is it!");
+if(time > 4 &&  time < 12){
+
+    alert('Good Morning');
+}
+
+else if(time > 12 &&  time < 17){
+
+    alert('Good Afternoon');
+}
+
+else if(time > 12 &&  time < 17){
+
+    alert('Good Evening');
+}
+
+else {
+    alert( 'Good Night');
+}
